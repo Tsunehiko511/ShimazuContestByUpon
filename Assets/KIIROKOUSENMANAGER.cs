4973e38bf38270544d1fb0b6fb5d0e0e437c26b7
Upon1227 <68001341+Upon1227@users.noreply.github.com>
2020-12-20 01:46:15 +0900

    aa

--- Assets/KIIROKOUSENMANAGER.cs
+++ Assets/KIIROKOUSENMANAGER.cs
@@ -7,37 +7,77 @@ public class KIIROKOUSENMANAGER : MonoBehaviour
     bool iesu;
     Animator animator,animator1,animator2;
     public GameObject kiiro1, kiiro2, kiiro3;
+    bool modore;
     // Start is called before the first frame update
     void Start()
     {
-        Animator animator = kiiro1.GetComponent<Animator>();
-        Animator animator1 = kiiro2.GetComponent<Animator>();
-        Animator animator2 = kiiro3.GetComponent<Animator>();
+         animator = kiiro1.GetComponent<Animator>();
+         animator1 = kiiro2.GetComponent<Animator>();
+         animator2 = kiiro3.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        modore = kiirobutton.modosu;
         iesu = kiirobutton.iio;
         Debug.Log(iesu);
+        Debug.Log(modore + "modore");
         if(iesu == true)
         {
-            animator.SetBool("kiiro", true);
+            kiirobutton.iio = false;
+            kiiro();
+        }
+        if(modore == true)
+        {
+            kiirobutton.modosu = false;
+            modoru();
+        }
+    }
+
+    void kiiro()
+    {
+        animator.SetBool("modoru", false);
+        animator1.SetBool("modoru", false);
+        animator2.SetBool("modoru", false);
+        animator.SetBool("kiiro", true);
             StartCoroutine(one());
             StartCoroutine(second());
-        }
+        
+    }
+
+    void modoru()
+    {
+        animator.SetBool("kiiro", false);
+        animator1.SetBool("kiiro", false);
+        animator2.SetBool("kiiro", false);
+       // Debug.Log("iiiii");
+       // animator.SetBool("modoru", true);
+       // StartCoroutine(modoruone());
+       // StartCoroutine(modorusecond());
     }
 
     IEnumerator one()
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(1);
         animator1.SetBool("kiiro", true);
         Debug.Log(1);
     }
     IEnumerator second()
     {
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(2);
         animator2.SetBool("kiiro", true);
         Debug.Log(2);
+    }
+
+    IEnumerator modoruone()
+    {
+        yield return new WaitForSeconds(2);
+        animator1.SetBool("modoru", true);
+    }
+    IEnumerator modorusecond()
+    {
+        yield return new WaitForSeconds(3);
+        animator2.SetBool("modoru", true);
     }
 }
--- Assets/kiirobutton.cs
+++ Assets/kiirobutton.cs
@@ -7,6 +7,7 @@ public class kiirobutton : MonoBehaviour
     AudioSource audio;
     public AudioClip sound;
     public static bool iio;
+    public static bool modosu;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +17,22 @@ public class kiirobutton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(iio);
+        Debug.Log(iio + "iio");
+        Debug.Log(modosu + "modosu");
     }
 
     void OnTriggerStay(Collider collider)
     {
         if(collider.gameObject.tag == "Player")
         {
-            iio = true;
-            transform.position = new Vector3(-14.4f, 22.5f, -1.13f);
+
+            transform.localScale = new Vector3(1, 0.5f, 1);
 
         }
         else
         {
-            iio = false;
-            transform.position = new Vector3(-14.4f, 23f, -1.13f);
+
+            transform.localScale = new Vector3(1, 0.75f, 1);
         }
       
     }
@@ -39,7 +41,17 @@ public class kiirobutton : MonoBehaviour
     {
         if(collider.gameObject.tag == "Player")
         {
+            iio = true;
             audio.PlayOneShot(sound);
+        }
+   
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            modosu = true;
         }
     }
 }
